6522f52b4ae6aec740d3b0fd064a560b385d4643
kenriki <riki.kenmochi@gmail.com>
2022-02-25 22:29:01 +0900

    レイアウト修正

--- src/views/content1.jsx
+++ src/views/content1.jsx
@@ -6,10 +6,11 @@ class Content1 extends React.Component {
     return (
       <div style={{ textAlign: "center" }}>
         <div>
-          <img src="https://www.his-j.com/kokunai/chubu/tour/slides/oka/037.jpg" class="css-img-style css-width" />
+          <img src="https://www.his-j.com/kokunai/chubu/tour/slides/oka/037.jpg" class="css-img-style" />
         </div>
         <div>
-          <p>エンジニアのためのプログラミング(Java, JavaScript, HTML5, CSS3, Pythonなど)等の情報まとめサイトです。いろいろな情報を共有して、より良い現場ライフになることが狙いです。</p>
+          <p>エンジニアのためのプログラミング(Java, JavaScript, HTML5, CSS3, Pythonなど)等の情報まとめサイトです。</p>
+          <p>いろいろな情報を共有して、より良い現場ライフになることが狙いです。</p>
           <div class="fb-share-button" data-href="https://kenriki.github.io/sample/" data-layout="button_count" data-size="small"
             data-mobile-iframe="true">
             <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fkenriki.php.xdomain.jp%2Fdoc%2F&amp;src=sdkpreparse"
